6944133153c4e20f9ffc341b07cb428cb5a635e0
Marco Hyyryläinen <marco@wheresmar.co>
2014-10-22 20:21:03 +0200

    Updated to right db table

--- Settings.php
+++ Settings.php
@@ -2,7 +2,7 @@
   class Settings {
   	public static $DB_USERNAME = "root";
   	public static $DB_PASSWORD = 'root';
-  	public static $DB_CONNECTION = 'mysql:host=localhost;dbname=php-projekt';
+  	public static $DB_CONNECTION = 'mysql:host=localhost;dbname=scotchbox';
     public static $ROOT_PATH = '/';
   	public static $DO_DEBUG = true;
   	public static $ERROR_LOG = "myerrors.log";
